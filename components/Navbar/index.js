9acc811e57b67aab914cc34421421b536e6bea86
Ali <raza8r@gmail.com>
2022-12-11 22:41:53 +0500

    setup variables

---
+++ components/Navbar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Navbar.module.scss";
+
+const Navbar = () => {
+    return (
+        <div className={styles.navbarWrapper}>
+            <h1>Navbar</h1>
+        </div>
+    );
+};
+
+export default Navbar;
