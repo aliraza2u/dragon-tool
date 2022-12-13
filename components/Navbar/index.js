eb5dd15f1ff4acf4040bdbdb95a27b8c327983a7
Muhammad Waseem <muhammadwaseem2u@gmail.com>
2022-12-12 22:35:15 -0800

    navbar are completed

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,12 +1,34 @@
 import React from "react";
 import styles from "./Navbar.module.scss";
-
+import Image from "next/image";
+import index from "../CustomButton/index";
+import CustomButton from "../CustomButton/index";
+import dragonWhiteLogo from "../../public/dragonWhiteLogo.png";
+import login from "../../public/login.png";
 const Navbar = () => {
-    return (
-        <div className={styles.navbarWrapper}>
-            <h1>Navbar</h1>
+  return (
+    <div className={styles.navbarWrapper}>
+      <div className={styles.navbar}>
+        <div className={styles.imageWrap}>
+          <Image src={dragonWhiteLogo} className={styles.wrap} />
         </div>
-    );
+        <div className={styles.text}>
+          <div className={styles.buyText}>
+            <span className={styles.bySub}>Buy Subscriptions</span>
+          </div>
+          <div className={styles.buttonWrap}>
+            <div className={styles.login}>
+              <Image src={login} className={styles.vector} />
+            </div>
+            <CustomButton children={"Member Area"} className={styles.area} />
+          </div>
+          <div className={styles.sign}>
+            <CustomButton children={"Signup Today"} className={styles.signup} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
