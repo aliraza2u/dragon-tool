b25f2a7d88460090da3bef3fef664bf6968f741a
Muhammad Waseem <muhammadwaseem2u@gmail.com>
2022-12-20 09:22:51 -0800

    importantDragon are clear

--- components/ImportantDragon/index.js
+++ components/ImportantDragon/index.js
@@ -4,18 +4,24 @@ import CustomButton from "../CustomButton/index";
 const ImportantDragon = () => {
   return (
     <div className={styles.importantDragonWrapper}>
-      <div className={styles.importantDragon}>
-        <h1>Important !</h1>
-        <p>
-          If You are Confused or Anyhow Hesitating to pay us. Contact the Live
-          Chat Support & Ask for a Anydesk overview . For More Details Contact
-          Chat Support.
-        </p>
-      </div>
-      <div className={styles.important}>
-        <CustomButton children={"CHAT NOW"} className={styles.wraper} />
-        <CustomButton children={"SIGN UP"} className={styles.wrap} />
-        <span>No payment needed</span>
+      <div className={styles.dragonWrap}>
+        <div className={styles.importantDragon}>
+          <h1>Important !</h1>
+          <p>
+            If You are Confused or Anyhow Hesitating to pay us. Contact the Live
+            Chat Support & Ask for a Anydesk overview . For More Details Contact
+            Chat Support.
+          </p>
+        </div>
+        <div className={styles.important}>
+          <CustomButton children={"CHAT NOW"} className={styles.wraper} />
+          <CustomButton children={"SIGN UP"} className={styles.wrap} />
+        </div>
+        <div className={styles.payment}>
+          <div className={styles.paymentNeeded}>
+            <p>No payment needed</p>
+          </div>
+        </div>
       </div>
     </div>
   );
